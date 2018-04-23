e9501e22730b4723b9c97cc602d564ee68cd343a
rafaelsilva <foxraf3d@gmail.com>
2018-04-23 13:58:47 -0300

    Aula 3  - Curso Appium

--- src/test/java/br/ce/rafaelsilva/appium/FormularioTest.java
+++ src/test/java/br/ce/rafaelsilva/appium/FormularioTest.java
@@ -1,7 +1,10 @@
 package br.ce.rafaelsilva.appium;
 
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -10,6 +13,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
 
 public class FormularioTest {
 
@@ -37,7 +42,13 @@ public class FormularioTest {
             }
         }
 
-        //Escrever
+        //Escrever nome
+        MobileElement campoNome = (MobileElement) driver.findElement(MobileBy.AccessibilityId("nome"));
+        campoNome.sendKeys("Rafael Simplício");
+
+        //Checar nome escrito
+        String text = campoNome.getText();
+        assertEquals("Rafael Simplício", text);
 
         driver.quit();
     }
