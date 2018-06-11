56dbc0942e09d007940861b370b2b9c79fb12086
rafael.silva <foxraf3d@gmail.com>
2018-06-11 18:55:25 -0300

    Tentando clique longo - Appium 1.3.2

--- src/test/java/br/ce/rafaelsilva/appium/Page/CliquesPage.java
+++ src/test/java/br/ce/rafaelsilva/appium/Page/CliquesPage.java
@@ -1,17 +1,24 @@
 package br.ce.rafaelsilva.appium.Page;
 import static br.ce.rafaelsilva.appium.core.DriverFactory.getDriver;
 import br.ce.rafaelsilva.appium.core.BasePage;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.touch.TouchActions;
 
 public class CliquesPage extends BasePage {
 
     public void cliqueLongo(){
     	WebElement elemento = getDriver().findElement(By.xpath("//*[@text='Clique Longo']"));
-    	Actions action = new Actions(getDriver());
-    	action.clickAndHold(elemento); 
+    	//Actions action = new Actions(getDriver());
+    	//action.clickAndHold(elemento);
+
+        /*TouchAction action = new TouchAction(getDriver());
+        action.longPress((LongPressOptions) elemento).perform();*/
+
+        TouchActions action = new TouchActions(getDriver());
+        action.longPress(elemento);
+        action.perform();
+
         
     }
     
