0316279ddb5ecd5fd5d1ee07abb62204691d1f17
rafael.silva <foxraf3d@gmail.com>
2018-07-04 18:13:58 -0300

    Aula 44 - Desafio - Implementando

---
+++ src/test/java/br/ce/rafaelsilva/appium/Test/SBTeste.java
@@ -0,0 +1,14 @@
+package br.ce.rafaelsilva.appium.Test;
+
+import br.ce.rafaelsilva.appium.core.BaseTest;
+
+public class SBTeste extends BaseTest {
+
+    /*
+    * Inserir Conta
+    * Excluir Conta
+    * Validações da Movimentação
+    * Exclusão + Saldo
+    */
+
+}
